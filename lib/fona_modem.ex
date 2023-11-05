afa13dd2778e776db1fabc3fdc1dacbfb7c13115
Bryan Green <twistedogre@gmail.com>
2023-11-05 00:48:03 -0700

    Init with mix new fona_modem

---
+++ lib/fona_modem.ex
@@ -0,0 +1,18 @@
+defmodule FonaModem do
+  @moduledoc """
+  Documentation for `FonaModem`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FonaModem.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fona_modem_test.exs
@@ -0,0 +1,8 @@
+defmodule FonaModemTest do
+  use ExUnit.Case
+  doctest FonaModem
+
+  test "greets the world" do
+    assert FonaModem.hello() == :world
+  end
+end
